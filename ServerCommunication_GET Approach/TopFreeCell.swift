387883e25a180ed048d6d488f875c3f03c1b65b8
Alivelu <alivelu7193@gmail.com>
2019-07-04 10:47:33 +0530

    first commit

---
+++ ServerCommunication_GET Approach/TopFreeCell.swift
@@ -0,0 +1,30 @@
+//
+//  TopFreeCell.swift
+//  ServerCommunication_GET Approach
+//
+//  Created by Alivelu Ravula on 4/19/18.
+//  Copyright © 2018 Roja. All rights reserved.
+//
+
+import UIKit
+
+class TopFreeCell: UITableViewCell {
+
+    @IBOutlet weak var appImg: UIImageView!
+    
+    @IBOutlet weak var appNameLbl: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ServerCommunication_GET Approach/ViewController.swift
+++ ServerCommunication_GET Approach/ViewController.swift
@@ -10,6 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    var URLSessionObj: URLSession?
+    var URLReq: URLRequest?
+    var dataTask: URLSessionDataTask?
+    
+    @IBAction func topPaidBtnTap(_ sender: Any) {
+        
+    self.URLSessionObj = URLSession(configuration: .default)
+    self.URLReq = URLRequest(url: URL(string: "https://rss.itunes.apple.com/api/v1/in/ios-apps/top-free/all/10/explicit.json")!)
+    self.dataTask = self.URLSessionObj?.dataTask(with: self.URLReq!, completionHandler: { (data, resp, error) in
+            
+            print(data)
+        do{
+            var dict = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:Any]
+//            print(dict)
+            var feedDict : [String:Any] = dict["feed"] as! [String:Any]
+//            print(feedDict)
+            var resultsArr : [[String:Any]]  = feedDict["results"] as! [[String:Any]]
+//            print(resultsArr)
+            
+            for i in 0..<resultsArr.count
+            {
+            var appDict : [String:Any]  = resultsArr[i]
+                print(appDict["name"]!)
+            }
+            
+        }
+        catch{
+            print("something")
+        }
+        
+        })
+        
+        self.dataTask?.resume()
+        
+        let topFreeTVC = self.storyboard?.instantiateViewController(withIdentifier: "TopFreeTableVC")
+        self.navigationController?.pushViewController(topFreeTVC!, animated: true)
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
